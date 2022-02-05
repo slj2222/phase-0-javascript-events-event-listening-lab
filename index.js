a23c6a4fa1b32ec73bc395fa9148a49d01a74b6b
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-04 18:14:48 -0600

    commit tree 2284aaed813bd999eaf8ea7099a03baaccc95e77

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// function addingEventListener() {
+//     input = document.getElementById("input");
+
+//     function clickAlert() {
+//         alert('I was clicked!');
+//       }
+      
+//       input.addEventListener('click', clickAlert);
+
+// }
+
+// document.getElementById("input").addingEventListener('click',() => alert("I was clicked!")) 
+
+
+// 
+
 function addingEventListener() {
     input = document.getElementById("input");
 
